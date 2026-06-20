21ceef87257ef35f438ed987a162b2a925b7c5bb
César Mora <cesar.mcid@gmail.com>
2026-06-19 21:15:24 -0400

    [wip] Agregar configuracion hmr

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,24 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import { bunny } from 'laravel-vite-plugin/fonts';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-            fonts: [
-                bunny('Instrument Sans', {
-                    weights: [400, 500, 600],
-                }),
-            ],
-        }),
-        tailwindcss(),
-    ],
     server: {
         host: '0.0.0.0',
         hmr: {
             host: 'localhost'
         },
+        allowedHosts: ['.udec.cl'],
         watch: {
             usePolling: true,
             ignored: ['**/node_modules/**', '**/vendor/**'],
         },
     },
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+        tailwindcss(),
+    ],
 });
