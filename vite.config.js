4ff289144821286eb8d56792f498db3033b759a1
César Mora <cesar.mcid@gmail.com>
2026-06-19 20:57:17 -0400

    [wip] Agregar configuracion hmr

--- vite.config.js
+++ vite.config.js
@@ -17,8 +17,13 @@ export default defineConfig({
         tailwindcss(),
     ],
     server: {
+        host: '0.0.0.0',
+        hmr: {
+            host: 'localhost'
+        },
         watch: {
-            ignored: ['**/storage/framework/views/**'],
+            usePolling: true,
+            ignored: ['**/node_modules/**', '**/vendor/**'],
         },
     },
 });
